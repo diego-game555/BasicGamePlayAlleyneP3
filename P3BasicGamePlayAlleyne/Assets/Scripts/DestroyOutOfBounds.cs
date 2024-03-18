284002c9ce6dbb0c30701fa604d1f5175b67061b
Diego Alleyne <714289@student.dusd.net>
2024-03-18 11:40:02 -0700

    Animals Spawn
    Made animals spawn and added collider so they will be deleted from impact of pizza 3/18

--- P3BasicGamePlayAlleyne/Assets/Scripts/DestroyOutOfBounds.cs
+++ P3BasicGamePlayAlleyne/Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,7 +10,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,8 +19,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        }
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- P3BasicGamePlayAlleyne/Assets/Scripts/PlayerController.cs
+++ P3BasicGamePlayAlleyne/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public float zRange = 10;
+    public float zMin;
+    public float zMax;
+
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
@@ -28,6 +33,17 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward *  verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
